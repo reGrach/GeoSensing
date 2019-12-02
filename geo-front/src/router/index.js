59ff2b60ca7bcabf750d37fb9fec8be46c7fb9fb
EberhardFaber <makssup32@gmail.com>
2019-12-03 00:03:53 +0300

    add_icon

--- geo-front/src/router/index.js
+++ geo-front/src/router/index.js
@@ -1,10 +1,23 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Geo from '../views/Geo.vue'
+import GeoNew from '../views/GeoNew.vue'
+
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/geo',
+    name: 'geo',
+    component: Geo
+  },
+  {
+    path: '/geonew',
+    name: 'geonew',
+    component: GeoNew
+  },
   {
     path: '/',
     name: 'home',
