13e6d98dee5d8c2d64c391e42c0bcc928185ff6f
reGrach <germangrach.94@yandex.ru>
2019-12-04 01:27:45 +0300

    init auth

--- geo-front/src/router/index.js
+++ geo-front/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '@/views/Home.vue'
+import Authentication from '@/views/Authentication'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'home',
     component: Home
+  },
+  {
+    path: '/login',
+    name: 'Authentication',
+    component: Authentication
   },
   {
     path: '/about',
