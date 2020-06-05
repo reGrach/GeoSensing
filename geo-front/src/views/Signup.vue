0428be1d4b9d51566aeac4bbbb393907042e93ee
EberhardFaber <makssup32@gmail.com>
2020-06-06 00:59:03 +0300

    Merge branch 'develop'

--- geo-front/src/views/Signup.vue
+++ geo-front/src/views/Signup.vue
@@ -57,7 +57,7 @@ export default {
           login: this.login,
           password: this.password
         })
-        .then(() => this.$router.push({ name: 'signin' }))
+        .then(() => this.$router.push('/signin'))
     }
   }
 }
