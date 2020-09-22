2c652763180d02f7c4f00e9dcb233994e90789ba
reGrach <germangrach.94@yandex.ru>
2020-09-23 02:12:02 +0300

    add preloader

--- geo-front/src/App.vue
+++ geo-front/src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <v-app>
     <app-header></app-header>
-    <v-main>
-      <router-view/>
+    <v-main >
+      <router-view v-show="!showPreloader" />
+      <Preloader v-if="showPreloader"></Preloader>
     </v-main>
     <app-footer></app-footer>
   </v-app>
@@ -11,11 +12,17 @@
 <script>
 import AppHeader from '@/components/base/AppHeader.vue';
 import AppFooter from '@/components/base/AppFooter.vue';
+import Preloader from '@/components/base/Preloader.vue';
+import { mapGetters } from 'vuex';
 
 export default {
   components: {
     AppHeader,
     AppFooter,
+    Preloader,
+  },
+  computed: {
+    ...mapGetters(['showPreloader']),
   },
 };
 </script>
--- geo-front/src/store/mutationsType.js
+++ geo-front/src/store/mutationsType.js
@@ -3,6 +3,8 @@ export const PURGE_AUTH = 'PURGE_AUTH';
 export const SET_ERROR = 'SET_ERROR';
 export const PURGE_ERROR = 'PURGE_ERROR';
 export const SET_PROCESSING = 'SET_PROCESSING';
+export const SHOW_PRELOADER = 'SHOW_PRELOADER';
+export const HIDE_PRELOADER = 'HIDE_PRELOADER';
 
 export const SET_PROFILE = 'SET_PROFILE';
 export const UPLOAD_IMG = 'UPLOAD_IMG';
