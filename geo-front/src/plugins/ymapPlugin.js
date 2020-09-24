cca864da4628316fc63f5142d32810fce48198c5
reGrach <germangrach.94@yandex.ru>
2020-09-24 22:52:15 +0300

    fix after merge

--- geo-front/src/plugins/ymapPlugin.js
+++ geo-front/src/plugins/ymapPlugin.js
@@ -1,11 +1,11 @@
-import Vue from 'vue'
-import YmapPlugin from 'vue-yandex-maps'
+import Vue from 'vue';
+import YmapPlugin from 'vue-yandex-maps';
 
 const settings = {
-    apiKey: '989c3b6b-bc85-4d4e-9c43-9ad29db3c71e',
-    lang: 'ru_RU',
-    coordorder: 'latlong',
-    version: '2.1'
-  }
+  apiKey: '989c3b6b-bc85-4d4e-9c43-9ad29db3c71e',
+  lang: 'ru_RU',
+  coordorder: 'latlong',
+  version: '2.1',
+};
 
 Vue.use(YmapPlugin, settings);
