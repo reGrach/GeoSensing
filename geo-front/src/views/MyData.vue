46f57956c9043189dc495cda124f748187ae3710
Vetrandir <vetrandir@gmail.com>
2020-09-25 00:15:49 +0300

    add tabs and datatable

--- geo-front/src/views/MyData.vue
+++ geo-front/src/views/MyData.vue
@@ -1,43 +1,51 @@
 <template>
-  <v-container fill-height fluid>
-    <v-sheet id="app">
-      <yandex-map :coords="coords" :zoom="10" @click="onClick">
-        <ymap-marker marker-id="123" :coords="coords1" :balloon-template="balloonTemplate" />
-      </yandex-map>
-    </v-sheet>
-  </v-container>
+  <v-card tile class="elevation-0">
+    <v-tabs v-model="tab" color="indigo" flat grow>
+      <v-tab>Карта</v-tab>
+      <v-tab>Таблица</v-tab>
+    </v-tabs>
+    <v-tabs-items v-model="tab">
+      <v-tab-item>
+        <MapData :points="points"> </MapData>
+      </v-tab-item>
+      <v-tab-item>
+        <TableData :points="points"> </TableData>
+      </v-tab-item>
+    </v-tabs-items>
+  </v-card>
 </template>
 
 <script>
-import { yandexMap, ymapMarker } from 'vue-yandex-maps';
-
+import MapData from '../components/cards/MapData.vue';
+import TableData from '../components/cards/TableData.vue';
 export default {
-  components: { yandexMap, ymapMarker },
+  components: { MapData, TableData },
 
   data: () => ({
-    coords: [60, 30],
-    coords1: [0, 0],
+    tab: null,
+    points: [
+      {
+        number: 1,
+        author: "Дима",
+        x:60.44906,
+        y:30.289094,
+        coords:[60.44906, 30.289094]
+      },
+      {
+        number: 2,
+        author: "Миша",
+        x:60.448902,
+        y:30.28967,
+        coords:[60.448902, 30.28967]
+      },
+      {
+        number: 3,
+        author: "Юля",
+        x:60.449111,
+        y:30.290246,
+        coords:[60.449111, 30.290246]
+      }
+    ]
   }),
-  computed: {
-    balloonTemplate() {
-      return '<h1>Хей, не тыкай так сильно, больно вообще-то</h1><p>Я вот тебе координаты даю: ${this.coords1}, а ты мне?(((</p><img src="https://ru.meming.world/images/ru/thumb/7/73/%D0%A8%D0%B0%D0%B1%D0%BB%D0%BE%D0%BD_%D0%BA%D0%BE%D1%82.jpg/300px-%D0%A8%D0%B0%D0%B1%D0%BB%D0%BE%D0%BD_%D0%BA%D0%BE%D1%82.jpg">';
-    },
-  },
-  methods: {
-    onClick(e) {
-      this.coords1 = e.get('coords');
-    },
-  },
 };
 </script>
-
-<style scoped>
-#app {
-  width: 100%;
-  height: 94vh;
-}
-
-.ymap-container {
-  height: 94%;
-}
-</style>
