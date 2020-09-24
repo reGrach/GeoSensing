656e2270b0b752882c57988c0a70732ee8697d35
Vetrandir <vetrandir@gmail.com>
2020-09-25 00:30:57 +0300

    key seq

--- geo-front/src/plugins/ymapPlugin.js
+++ geo-front/src/plugins/ymapPlugin.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import YmapPlugin from 'vue-yandex-maps';
 
 const settings = {
-  apiKey: '989c3b6b-bc85-4d4e-9c43-9ad29db3c71e',
+  apiKey: process.env.YMAPKEY,
   lang: 'ru_RU',
   coordorder: 'latlong',
   version: '2.1',
