3b1a80ad51fabc2744e7b8b1c43612fe0b319538
reGrach <germangrach.94@yandex.ru>
2020-09-25 20:14:19 +0300

    expamle work with store (expirements)

--- geo-front/src/views/Experiment.vue
+++ geo-front/src/views/Experiment.vue
@@ -1,12 +1,43 @@
 <template>
-<fixations></fixations>
+  <v-container fluid>
+    <v-select
+      v-model="currentExp"
+      :items="expirement"
+      item-value="id"
+      item-text="title"
+      label="Выберите эксперимент"
+      required
+      :loading="getProcessing"
+    ></v-select>
+      <fixations v-if="canFixPoint"></fixations>
+  </v-container>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 import Fixations from '../components/cards/Fixations.vue';
+import { GET_LIST_EXP } from '../store/actionsType';
 
 export default {
   components: { Fixations },
-  data: () => ({}),
+  data: () => ({
+    expirement: [],
+    currentExp: null,
+  }),
+
+  computed: {
+    ...mapGetters(['isEasyUser', 'getProcessing']),
+    canFixPoint() {
+      return this.expirement.length > 0 && !this.isEasyUser;
+    },
+  },
+
+  created() {
+    if (!this.isEasyUser) {
+      this.$store.dispatch(GET_LIST_EXP).then((list) => {
+        this.expirement = list;
+      });
+    }
+  },
 };
 </script>
